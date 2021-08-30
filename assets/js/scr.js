1e23a0713db124e3eb26952d5a0ebb713b5f0990
Glitchii <darlionthesis@gmail.com>
2021-08-30 13:52:39 -0700

    Update scr.js

--- assets/js/scr.js
+++ assets/js/scr.js
@@ -1,3 +1,4 @@
-window.onload = () => {
+onload = () => {
+    if (new URL(location).searchParams.get('troy') !== null) location.href = 'https://discord.com/channels/881689539192713256/881689845985054741/881990996836118548';
     document.querySelector('img').onerror = el => el.target.style.opacity = 0;
 }
