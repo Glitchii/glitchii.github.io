8ea3cc8ffdbedc751624770869aedb6ebe2b0d72
Glitchii <glitchii@google.com>
2022-11-26 23:41:29 +0000

    Update contact details

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,18 +2,19 @@
 addEventListener('DOMContentLoaded', () => {
     document.querySelector('form')?.addEventListener('submit', e => {
         e.preventDefault();
-        let name = document.querySelector('form .field.name input').value,
-            email = document.querySelector('form .field.email input').value,
-            mine = 'glitchii@tempfile.site',
-            body = `${document.querySelector('form .field.message textarea').value}\n\n`,
-            subject = `Message from ${name || email || location.href}`;
+
+        const name = document.querySelector('form .field.name input').value
+        const email = document.querySelector('form .field.email input').value
+        const mine = `darlion.thesis+${location.hostname}@gmail.com`
+        const body = `${document.querySelector('form .field.message textarea').value}\n\n`
+        const subject = `Message from ${name || email || location.href}`
 
         if (!name && email) body += email;
         else if (name) {
             body += (name || email)
             email && (body += ` (${email})`);
         }
+        
         open(`mailto:${mine}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body + `\nSent from ${location.href}`)}`);
-
     });
 });
